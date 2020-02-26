18cfacecf70c063f49f0bc38de1436dda1c49a3e
yelo <zhihuzeye@gmail.com>
2020-02-27 03:29:14 +0800

    feat: auto execute cronjob

--- packages/sayhi-cronjob/src/index.js
+++ packages/sayhi-cronjob/src/index.js
@@ -2,6 +2,12 @@ const redis = require('./redis')
 
 const CACHE_KEY = 'job'
 
-module.exports = async () => {
+const main = async () => {
   await redis.set(CACHE_KEY, (new Date()).toUTCString())
+}
+
+module.exports = main
+
+if (!module.parent) {
+  main()
 }
