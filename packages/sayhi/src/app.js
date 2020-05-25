a5f904911ba8796bfde4a3d4abb9b215e3017b12
yelo <zhihuzeye@gmail.com>
2020-05-25 16:25:20 +0800

    feat: add delay for sayhi

--- packages/sayhi/src/app.js
+++ packages/sayhi/src/app.js
@@ -1,8 +1,12 @@
+const random = require('random')
+const delay = require('delay')
 const redis = require('./redis')
 
 const CACHE_KEY = 'job'
+const DELAY_MAXIMUM = process.env.APP_DELAY_MAXIMUM || (2 * 1000)
 
 module.exports = async () => {
   const job = await redis.get(CACHE_KEY)
+  await delay(random.int(0, DELAY_MAXIMUM))
   return `Hi. (Last Job: ${job})`
 }
