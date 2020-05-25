53ebc428155c132cf705174522b6cd9f7bdf111b
yelo <zhihuzeye@gmail.com>
2020-05-25 17:03:08 +0800

    fix: typo

--- packages/sayhi-cronjob/src/redis.js
+++ packages/sayhi-cronjob/src/redis.js
@@ -1,7 +1,7 @@
 const Redis = require('ioredis')
 
 const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1'
-const REDIS_PORT = process.env.REIDS_PORT || 6379
+const REDIS_PORT = process.env.REDIS_PORT || 6379
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD || ''
 const REDIS_DB = process.env.REDIS_DB || 0
 
