5a3f3a872f870f7180e975c7a660b5d6f0344caa
yelo <zhihuzeye@gmail.com>
2020-05-27 15:09:40 +0800

    feat: handle env var type

--- packages/sayhi/src/env.js
+++ packages/sayhi/src/env.js
@@ -1,6 +1,6 @@
 exports.REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1'
-exports.REDIS_PORT = process.env.REDIS_PORT || 6379
+exports.REDIS_PORT = +process.env.REDIS_PORT || 6379
 exports.REDIS_PASSWORD = process.env.REDIS_PASSWORD || ''
-exports.REDIS_DB = process.env.REDIS_DB || 0
+exports.REDIS_DB = +process.env.REDIS_DB || 0
 
 exports.CACHE_KEY = process.env.APP_CACHE_KEY || 'job'
