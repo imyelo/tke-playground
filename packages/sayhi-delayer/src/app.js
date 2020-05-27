f9ec7bd3a4cc63d7122522899de0f62a325636c5
yelo <zhihuzeye@gmail.com>
2020-05-27 11:15:41 +0800

    feat(delayer): handle istio common headers

--- packages/sayhi-delayer/src/app.js
+++ packages/sayhi-delayer/src/app.js
@@ -1,12 +1,14 @@
 const random = require('random')
 const delay = require('delay')
 const got = require('got')
+const { getIstioRequestHeaders } = require('./istio')
 
 const TARGET = process.env.APP_TARGET || 'http://sayhi/'
 const DELAY_MAXIMUM = process.env.APP_DELAY_MAXIMUM || (2 * 1000)
 
-module.exports = async () => {
+module.exports = async (request) => {
   await delay(random.int(0, DELAY_MAXIMUM))
-  const response = await got(TARGET)
+  const headers = getIstioRequestHeaders(request)
+  const response = await got(TARGET, { headers })
   return response.body
 }
