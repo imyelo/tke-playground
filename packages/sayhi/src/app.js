4bb206d0e1f58935aa9e87979c93167537529d32
yelo <zhihuzeye@gmail.com>
2020-05-27 11:26:03 +0800

    feat(sayhi): remove delay builtin from sayhi

--- packages/sayhi/src/app.js
+++ packages/sayhi/src/app.js
@@ -1,10 +1,7 @@
-const random = require('random')
-const delay = require('delay')
 const redis = require('./redis')
-const { CACHE_KEY, DELAY_MAXIMUM } = require('./env')
+const { CACHE_KEY } = require('./env')
 
 module.exports = async () => {
   const job = await redis.get(CACHE_KEY)
-  await delay(random.int(0, DELAY_MAXIMUM))
   return `Hi. (Last Job: ${job})`
 }
--- packages/sayhi/src/env.js
+++ packages/sayhi/src/env.js
@@ -4,4 +4,3 @@ exports.REDIS_PASSWORD = process.env.REDIS_PASSWORD || ''
 exports.REDIS_DB = process.env.REDIS_DB || 0
 
 exports.CACHE_KEY = process.env.APP_CACHE_KEY || 'job'
-exports.DELAY_MAXIMUM = +process.env.APP_DELAY_MAXIMUM || (2 * 1000)
