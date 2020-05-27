9e26e6426786ad2a75421cc4dba34b3f604095e3
yelo <zhihuzeye@gmail.com>
2020-05-27 11:43:18 +0800

    feat: update default checker schedule

--- packages/sayhi-checker/src/env.js
+++ packages/sayhi-checker/src/env.js
@@ -1,2 +1,2 @@
 exports.SAYHI_ENDPOINT = process.env.SAYHI_ENDPOINT || 'http://127.0.0.1:3000/'
-exports.SCHEDULE = process.env.APP_SCHEDULE || '0 */5 * * * *'
+exports.SCHEDULE = process.env.APP_SCHEDULE || '*/5 * * * * *'
